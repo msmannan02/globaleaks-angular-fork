86dbf58f0973decb1c3940d58a3b06658f895eb7
iamuhammadumair <muhammadumair3344@gmail.com>
2023-03-20 14:58:19 +0500

    rxjs example

--- src/app/libs/select/select.component.ts
+++ src/app/libs/select/select.component.ts
@@ -1,37 +1,61 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-// import { Observable } from 'rxjs';
-// import '@ng-select/ng-select/themes/default.theme.css';
 
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+interface Post {
+	id: number;
+	title: string;
+}
 @Component({
-  selector: 'app-select',
-  templateUrl: './select.component.html',
-  styleUrls: ['./select.component.css'],
+	selector: 'app-select',
+	templateUrl: './select.component.html',
+	styleUrls: ['./select.component.css'],
 })
 export class SelectComponent implements OnInit {
-  selectedCity: any;
-  people: any;
-  peopleLoading = false;
-  selectedPeople = [{ name: 'optio molestias id quia eum' }];
-  defaultBindingsList = [
-    { value: 1, label: 'Vilnius' },
-    { value: 2, label: 'Kaunas' },
-    { value: 3, label: 'Pavilnys' },
-  ];
+	selectedCity: any;
+	people: any;
+	peopleLoading = false;
+	selectedPeople = [{ name: 'optio molestias id quia eum' }];
+	defaultBindingsList = [
+		{ value: 1, label: 'Vilnius' },
+		{ value: 2, label: 'Kaunas' },
+		{ value: 3, label: 'Pavilnys' },
+	];
 
-  ngOnInit() {
-    this.selectedCity = this.defaultBindingsList[0];
-    this.http
-      .get<any>('https://jsonplaceholder.typicode.com/posts', {
-        // headers: headers,
-      })
-      .subscribe((data) => {
-        this.people = data.map((row: any) => {
-          return { value: row.id, name: row.title };
-        });
-        this.peopleLoading = false;
-      });
-  }
+	ngOnInit() {
+		this.selectedCity = this.defaultBindingsList[0];
+		// this.getPostsByHttp().subscribe((data) => {
+		// 	this.people = data.map((row: any) => {
+		// 		return { value: row.id, name: row.title };
+		// 	});
+		// 	this.peopleLoading = false;
+		// });
+		this.getPostsWithRxjs().subscribe((posts) => {
+			this.people = posts;
+			this.peopleLoading = false;
+		});
+	}
 
-  constructor(private http: HttpClient) {}
+	getPostsWithRxjs(): Observable<any[]> {
+		return this.http
+			.get<Post[]>('https://jsonplaceholder.typicode.com/posts')
+			.pipe(
+				map((posts) => {
+					return posts.map((post) => {
+						return { value: post.id, name: post.title };
+					});
+				})
+			);
+	}
+
+	getPostsByHttp = () => {
+		return this.http.get<Post[]>(
+			'https://jsonplaceholder.typicode.com/posts',
+			{
+				// headers: headers,
+			}
+		);
+	};
+	constructor(private http: HttpClient) {}
 }
